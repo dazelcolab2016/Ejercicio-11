7941c99ef0886eb241b0797296ede259336c7d0f
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-27 17:34:05 -0500

    run PosNeg

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Ejercicio-11
+# Ejercicio - 11
+
+*Inicio: Post-Negative*
+
+## Ingreso datos
+- a
+- b
+- negative
+- posNeg
+
+
+## Proceso
+- obtener a
+- obtener b
+- obtener negative
+- si a < 0 y b > 0 y negative es = a true entonces
+    posNeg = 1
+    mostrar posNeg
+- si a > 0 y b < 0 y negative es = a true entonces
+    posNeg = 1
+    mostrar posNeg
+-y si no
+    posNeg = 0
+    mostrar posNeg
+
+
+
+fin si
+
+*fin*
+
+
---
+++ main.js
@@ -0,0 +1,42 @@
+function negativoA()
+{
+	var txt_negativo = document.getElementById("negativo-txt");
+	var checkA = document.getElementById("negativo_a");
+
+	if(checkA.checked)
+	{
+		txt_negativo.innerHTML = "SI";
+	}else{
+		txt_negativo.innerHTML = "NO";
+	}
+}
+
+function evaluar()
+{
+	var aNegative = document.getElementById("negativo_a").checked;
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+	var posNeg  = 0;
+        
+	if(a < 0 && b > 0 && aNegative == true )
+    {
+            posNeg  = 1;
+            salida.innerHTML = posNeg;
+        
+        }else if (a > 0 && b < 0 && aNegative == true )
+        {
+            posNeg  = 1;
+            salida.innerHTML = posNeg;
+        }else
+        {
+            posNeg  = 0;
+            salida.innerHTML = posNeg;
+        }
+}
+
+
+
+
+
+
+
